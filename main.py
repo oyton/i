6042a39dc05ef634016228fcb73de4a5e2fa167c
oytun on laptop <oytunokt@gmail.com>
2022-02-22 09:48:38 +0300

    QThread called with wrong argument types: int error walk around #+2

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ class ThreadCapture(QThread):
     updateFrame = Signal(QImage)
 
     def __init__(self, sensor_id=0, parent=None):
-        super(QThread.__init__(self))
+        super(QThread, self).__init__()
         self.sensor_id = sensor_id
         self.status = True
         self.cap = cv2.VideoCapture(gstreamer_pipeline(sensor_id=self.sensor_id, 
