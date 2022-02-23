715b2d1bc91da060b9ae8a046e7b961a1d09ba0d
oytun jetson edge <oytunokt@gmail.com>
2022-02-23 12:12:06 +0300

    memleak temporarily resolved by qimage2ndarray repo

--- tests/memleak.py
+++ tests/memleak.py
@@ -1,6 +1,7 @@
 from re import A
 import numpy as np
 from PySide2.QtGui import QImage
+import qimage2ndarray
 
 def createImage():
     a = np.random.rand(500,500,3)
@@ -10,11 +11,12 @@ def createImage():
     return A
 
 
-for i in range(100):
+for i in range(1000):
     A = createImage()
-    ASTR = A.tostring()
-    h, w, ch = A.shape
-    QA = QImage(ASTR, w, h, QImage.Format_RGB888)
-    ASTR = b""
-    del ASTR
+#    ASTR = A.tostring()
+#    h, w, ch = A.shape
+#    QA = QImage(ASTR, w, h, QImage.Format_RGB888)
+#    ASTR = b""
+#    del ASTR
+    QA = qimage2ndarray.array2qimage(A)
 
