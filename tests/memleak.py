ca306eb96b37b0d98c0681ee49644efb3c688161
oytun on laptop <oytunokt@gmail.com>
2022-02-23 11:52:57 +0300

    adding tests to work on memory leak problem efficiently

---
+++ tests/memleak.py
@@ -0,0 +1,16 @@
+from re import A
+import numpy as np
+from PySide2.QtGui import QImage
+
+def createImage():
+    a = np.random.rand(500,500,3)
+    aa = 255*a 
+    aaa = np.floor(aa)
+    A = aaa.astype(np.uint8)
+    return A
+
+for i in range(100):
+    ASTR = A.tostring()
+    h, w, ch = A.shape
+    QA = QImage(ASTR, w, h, QImage.Format_RGB888)
+    
